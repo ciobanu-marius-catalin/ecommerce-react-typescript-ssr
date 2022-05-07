cbd1dadfeadee011ba02e50273550253a227979e
Catalin Ciobanu <ciobanu.m.catalin@gmaill.com>
2022-05-07 17:26:17 +0300

    Added eslint for jest

--- web-app/src/pages/frontend/home/index.test.tsx
+++ web-app/src/pages/frontend/home/index.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { HomePage } from "./index";
 
 describe("pages/frontend/home", () => {
